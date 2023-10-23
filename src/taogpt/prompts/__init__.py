861aa5ec6a3abe3f0b9fb518624fde8cbcf7e22e
winstonq <winstonq@zillowgroup.com>
2023-10-22 20:25:28 -0700

    refactor

--- src/taogpt/prompts/__init__.py
+++ src/taogpt/prompts/__init__.py
@@ -2,59 +2,38 @@ from __future__ import annotations
 import dataclasses as _dc
 import pathlib as _path
 
+_template_dir = _path.Path(__file__).parent
+
+
+def _read(path) -> str:
+    with open(path, 'r') as f:
+        return f.read()
+
 
 @_dc.dataclass
 class PromptDb:
-    tao_intro: str # main system prompt
-    tao_templates: str
-    tao_template_direct_step_answer: str
-    tao_template_notes_for_files: str
-    tao_template_intuitive_answer: str
-    orchestrator_ask_init_analysis: str
-    orchestrator_rank_choices: str
-    orchestrator_at_step: str
-    orchestrator_next_step: str
-    orchestrator_proceed: str
-    orchestrator_proceed_to_step: str
-    orchestrator_expand_parse_error: str
-    orchestrator_parse_error: str
-    orchestrator_prior_approaches: str
-    orchestrator_criticisms: str
-    orchestrator_error_noted: str
-    orchestrator_summarize: str
-    orchestrator_summarize_questions: str
-    sage_intro: str
-    sage_final_check: str
-    sage_blame: str
+    tao_intro: str = _read(_template_dir / 'tao.md')
+    tao_templates: str = _read(_template_dir / 'tao_templates.md')
+    tao_template_direct_step_answer: str = _read(_template_dir / 'tao_template_direct_step_answer.md')
+    tao_template_notes_for_files: str = _read(_template_dir / 'tao_template_notes_for_files.md')
+    tao_template_intuitive_answer: str = _read(_template_dir / 'tao_template_intuitive_answer.md')
+    orchestrator_ask_init_analysis: str = _read(_template_dir / 'orchestrator_ask_init_analysis.md')
+    orchestrator_rank_choices: str = _read(_template_dir / 'orchestrator_rank_choices.md')
+    orchestrator_at_step: str = _read(_template_dir / 'orchestrator_at_step.md')
+    orchestrator_next_step: str = _read(_template_dir / 'orchestrator_next_step.md')
+    orchestrator_proceed: str = _read(_template_dir / 'orchestrator_proceed.md')
+    orchestrator_proceed_to_step: str = _read(_template_dir / 'orchestrator_proceed_to_step.md')
+    orchestrator_expand_parse_error: str = _read(_template_dir / 'orchestrator_expand_parse_error.md')
+    orchestrator_parse_error: str = _read(_template_dir / 'orchestrator_parse_error.md')
+    orchestrator_prior_approaches: str = _read(_template_dir / 'orchestrator_prior_approaches.md')
+    orchestrator_criticisms: str = _read(_template_dir / 'orchestrator_criticisms.md')
+    orchestrator_error_noted: str = _read(_template_dir / 'orchestrator_error_noted.md')
+    orchestrator_summarize: str = _read(_template_dir / 'orchestrator_summarize.md')
+    orchestrator_summarize_questions: str = _read(_template_dir / 'orchestrator_summarize_questions.md')
+    sage_intro: str = _read(_template_dir / 'sage.md')
+    sage_final_check: str = _read(_template_dir / 'sage_final_check.md')
+    sage_blame: str = _read(_template_dir / 'sage_blame.md')
 
     @staticmethod
     def load_defaults():
-        path = _path.Path(__file__).parent
-
-        def _read(path):
-            with open(path, 'r') as f:
-                return f.read()
-
-        return PromptDb(
-            tao_intro=_read(path / 'tao.md'),
-            tao_templates=_read(path / 'tao_templates.md'),
-            tao_template_direct_step_answer=_read(path / 'tao_template_direct_step_answer.md'),
-            tao_template_notes_for_files=_read(path / 'tao_template_notes_for_files.md'),
-            tao_template_intuitive_answer=_read(path / 'tao_template_intuitive_answer.md'),
-            orchestrator_ask_init_analysis=_read(path / 'orchestrator_ask_init_analysis.md'),
-            orchestrator_rank_choices=_read(path / 'orchestrator_rank_choices.md'),
-            orchestrator_at_step=_read(path / 'orchestrator_at_step.md'),
-            orchestrator_next_step=_read(path / 'orchestrator_next_step.md'),
-            orchestrator_proceed=_read(path / 'orchestrator_proceed.md'),
-            orchestrator_proceed_to_step=_read(path / 'orchestrator_proceed_to_step.md'),
-            orchestrator_expand_parse_error=_read(path / 'orchestrator_expand_parse_error.md'),
-            orchestrator_parse_error=_read(path / 'orchestrator_parse_error.md'),
-            orchestrator_prior_approaches=_read(path / 'orchestrator_prior_approaches.md'),
-            orchestrator_criticisms=_read(path / 'orchestrator_criticisms.md'),
-            orchestrator_error_noted=_read(path / 'orchestrator_error_noted.md'),
-            orchestrator_summarize=_read(path / 'orchestrator_summarize.md'),
-            orchestrator_summarize_questions=_read(path / 'orchestrator_summarize_questions.md'),
-            sage_intro=_read(path / 'sage.md'),
-            sage_final_check=_read(path / 'sage_final_check.md'),
-            sage_blame=_read(path / 'sage_blame.md'),
-        )
+        return PromptDb()
